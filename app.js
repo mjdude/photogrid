5255ef95b6e70d9e83f624717624619fac4a16b4
mjdude <mo@syncspot.net>
2016-09-05 15:28:36 +0100

    added host and routes

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     path = require('path');
+    config = require('./config/config.js');
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
+app.set('host'. config.host);
 
 require('./routes/routes.js')(express, app);
 
