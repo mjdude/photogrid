65b0ef08a29cd8cf1d1636312cab94206d864b76
mjdude <mo@syncspot.net>
2016-09-05 16:44:01 +0100

    added randomly generated filename for pictures

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,34 @@
-module.exports = function(express , app){
+module.exports = function(express , app, formidable, fs, os){
   var router = express.Router();
 
   router.get('/', function(req, res, next){
     res.render('index', {host:app.get('host')});
   });
+
+router.post('/upload', function(req, res, next){
+  function generateFileName(filename){
+    var ext_regex = /(?:\.([^.]+))?$/;
+    var ext = ext_regex.exec(filename)[1];
+    var date = new Date().getTime();
+    var charBank = 'abcdefghijklmnopqrstuvwxyz';
+    var fstring = '';
+
+    for (var i = 0; i < 15; i++) {
+      fstring += charBank[parseInt(Math.random()*26)];
+    }
+
+    return (fstring += date + '.' + ext);
+  };
+
+  var tmpFile, nfile, fname;
+  var newForm = new formidable.IncomingForm();
+      newForm.keepExtensions = true;
+      newForm.parse(req, function(err, fields, files){
+        tmpFile = files.upload.path;
+        fname = '';
+      });
+
+});
 
   app.use('/', router);
 };
